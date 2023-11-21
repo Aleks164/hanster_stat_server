540b28c50cc5945f370e3dc226923d953b4c8de6
aleks164 <alekseyleha@mail.ru>
2023-11-21 21:32:15 +0300

    debug server

--- server/app.ts
+++ server/app.ts
@@ -20,12 +20,12 @@ import ratingByNmid from "./controller/byDateRange/byBarcode/rating";
 
 const app = express();
 const port = 443;
-const key = fs.readFileSync('/etc/letsencrypt/live/hansterstatserver.ru/privkey.pem');
-const cert = fs.readFileSync('/etc/letsencrypt/live/hansterstatserver.ru/fullchain.pem');
-const options = {
-  key: key,
-  cert: cert
-};
+// const key = fs.readFileSync('/etc/letsencrypt/live/hansterstatserver.ru/privkey.pem');
+// const cert = fs.readFileSync('/etc/letsencrypt/live/hansterstatserver.ru/fullchain.pem');
+// const options = {
+// key: key,
+// cert: cert
+// };
 
 app.use(cors());
 app.use("/orders", ordersByDateRange);
@@ -49,18 +49,18 @@ app.get("*", (req, res) => {
 //   console.log('endAt:', new Date());
 // }, 10000)
 
-const job = new CronJob('0 55 7 * * *', async function () {
-  console.log('Midnight1:', new Date());
-  await regularUpdateMongoDB();
-  console.log('Midnight2:', new Date());
-});
+// const job = new CronJob('0 55 6 * * *', async function () {
+//   console.log('Midnight1:', new Date());
+//   await regularUpdateMongoDB();
+//   console.log('Midnight2:', new Date());
+// });
 
 
-job.start();
+// job.start();
 
-const server = https.createServer(options, app);
+// const server = https.createServer(options, app);
 
-server.listen(port, async () => {
+app.listen(port, async () => {
   await connectToDB();
   console.log(`Listening on port ${port}`);
 });
